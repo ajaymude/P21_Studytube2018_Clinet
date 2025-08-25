5a341a520dfd6fe23f4c4cbdd08fb0c6fc6fe54f
ajaymude <mudeajay@gmail.com>
2025-08-25 11:45:31 +0530

    routes update

--- src/routes/MainRouter.jsx
+++ src/routes/MainRouter.jsx
@@ -9,13 +9,36 @@ import {
 
 // screens
 import App from "../App";
+import Home from "../pages/exams/Home";
+import SignUp from "../pages/auth/SignUp";
+import SignIn from "../pages/auth/SignIn";
+import Profile from "../pages/auth/Profile";
+import Exams from "../pages/exams/Exams";
+import Subjects from "../pages/exams/Subjects";
+import Chapters from "../pages/exams/Chapters";
+import Videos from "../pages/exams/Videos";
+import Player from "../pages/exams/Player";
+import Admin from "../pages/admin/Admin";
 
 const MainRouter = createBrowserRouter(
   createRoutesFromElements(
     <Route path="/" element={<App />}>
       {/* public routes */}
+      <Route path="/" index element={<Home />} />
+      <Route path="/signup" index element={<SignUp />} />
+      <Route path="/signin" index element={<SignIn />} />
+      <Route path="/exams" index element={<Exams />} />
+      <Route path="/subjects" index element={<Subjects />} />
+      <Route path="/signin" index element={<Chapters />} />
+      <Route path="/signin" index element={<Videos />} />
+
       {/* Registered users */}
+      <Route path="/me" index element={<Profile />} />
+      <Route path="/me" index element={<Player />} />
+
       {/* Admin users */}
+      <Route path="/me" index element={<Admin />} />
+
       {/* 404 fallback (must be last) */}
       <Route path="*" element={<h1 style={{ padding: 16 }}>Not found</h1>} />
     </Route>
